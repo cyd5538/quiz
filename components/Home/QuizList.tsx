92f2b403c86281e3fc0c08c52c61cb60a221ab70
cyd5538 <cyd5538@naver.com>
2024-07-19 08:40:33 +0900

    Feat: card
    글쓴이와 아바타 추가

--- components/Home/QuizList.tsx
+++ components/Home/QuizList.tsx
@@ -41,10 +41,32 @@ export function QuizLists({ quiz }: Props) {
           className="w-[320px] h-[250px] object-cover rounded-md"
         />
       </CardContent>
-      <CardHeader className="h-16 flex justify-center items-start mb-2">
-        <CardTitle>{truncateTitle(quiz.title, 28)}</CardTitle>
+      <CardHeader className="h-28 bg-zinc-100 flex flex-row items-center mb-2 p-4">
+        <CardTitle className="flex-1 text-lg font-bold">
+          {truncateTitle(quiz.title, 24)}
+        </CardTitle>
+        <div className="flex flex-col items-center">
+          <Image 
+            src={quiz.creator.profileImage}
+            width={45}
+            height={45}
+            alt={quiz.creator.displayName}
+            className="rounded-full mb-2"
+          />
+          <span className="text-sm">{quiz.creator.displayName}</span>
+        </div>
       </CardHeader>
-      <CardContent className="flex justify-between">
+      <CardFooter className="flex justify-between items-center pt-4 pb-[-4px] ">
+        <button className="px-2 py-2 rounded-xl border border-neutral-600 text-black bg-white hover:bg-gray-100 transition duration-200">
+          {quiz.difficulty}
+        </button>
+        <Button className="bg-zinc-800 hover:bg-zinc-950">
+          <Link href={`quiz/${quiz.id}`}>
+            Start Quiz
+          </Link>
+        </Button>
+      </CardFooter>
+      <div className="flex justify-between items-center h-12 p-6 b border-t-[1px] border-zinc-95s0">
         <p className="flex justify-center items-center gap-[4px]">
           <Heart size={24} color="black" fill="red" /> {quiz.likes}
         </p>
@@ -57,17 +79,7 @@ export function QuizLists({ quiz }: Props) {
             {quiz.commentCount}
           </p>
         </div>
-      </CardContent>
-      <CardFooter className="flex justify-between">
-        <button className="px-2 py-2 rounded-xl border border-neutral-600 text-black bg-white hover:bg-gray-100 transition duration-200">
-          {quiz.difficulty}
-        </button>
-        <Button className="bg-zinc-800 hover:bg-zinc-950">
-          <Link href={`quiz/${quiz.id}`}>
-            Start Quiz
-          </Link>
-        </Button>
-      </CardFooter>
+      </div>
     </Card>
   );
 }
