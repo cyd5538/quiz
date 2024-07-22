5ca8a07e2ad33cb3383ae112188d7fac3298ac92
cyd5538 <cyd5538@naver.com>
2024-07-22 14:52:36 +0900

    Feat: userstore
    loading 추가

--- stores/userStore.ts
+++ stores/userStore.ts
@@ -3,12 +3,16 @@ import { User } from 'firebase/auth'
 
 type userStoreType = {
   user: User | null
+  loading: boolean,
   setUser: (user: User | null) => void
+  setLoading: (user: boolean) => void
 }
 
 const userStore = create<userStoreType>((set) => ({
   user: null,
+  loading: true, 
   setUser: (user) => set({ user }),
+  setLoading: (loading) => set({ loading }),
 }))
 
 export default userStore
