b0023a7e82d8be028f1f24c723fde65797d0530e
cyd5538 <cyd5538@naver.com>
2024-07-22 10:02:26 +0900

    Design: signup page width 글자 크기 변경

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -44,27 +44,27 @@ export default function Page() {
  
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <Card className="w-[300px] flex flex-col items-center justify-center min-h-screen">
-        <CardHeader>
-          <CardTitle>서비스 이용하기</CardTitle>
-          <CardDescription>소셜 계정으로 간편하게 시작하세요.</CardDescription>
+      <Card className="sm:w-[600px] w-full flex flex-col items-center justify-center min-h-screen">
+        <CardHeader className="mb-6">
+          <CardTitle className="text-4xl">서비스 이용하기</CardTitle>
+          <CardDescription className="text-xl">소셜 계정으로 간편하게 시작하세요.</CardDescription>
         </CardHeader>
         <CardContent>
-          <div className="flex flex-col gap-4">
+          <div className="flex flex-col justify-start items-start gap-4">
             <Button
               onClick={handleGoogleLogin}
               className="w-full bg-white hover:bg-gray-100"
               variant="outline"
             >
               <FaGoogle className="mr-2 h-4 w-4" />
-              <span className="w-36">Google로 계속하기</span>
+              <span className="w-64">Google로 계속하기</span>
             </Button>
             <Button
               onClick={handleGithubLogin}
               className="w-full bg-[#24292e] text-white hover:bg-[#24292e]/90"
             >
               <FaGithub className="mr-2 h-4 w-4" />
-              <span className="w-36">GitHub로 계속하기</span>
+              <span className="w-64">GitHub로 계속하기</span>
             </Button>
           </div>
         </CardContent>
